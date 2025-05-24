69a076dfdb8ac13b7c96ed2d51f3cb30557e487f
Prashant Kumar <prashantkumar6425@gmail.com>
2025-05-24 16:01:02 +0530

    update component-index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,17 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Input from "./Input.jsx";
 import Logo from "./Logo";
+import Button from "./Button.jsx";
 import Container from './container/Container.jsx';
 import LogoutBtn from "./Header/LogoutBtn.jsx";
 
 export {
     Header,
     Footer,
+    Input,
     Logo,
+    Button,
     Container,
     LogoutBtn
 }
