0a677be086da73c90b0e5a8c49070d626d88896d
Prashant Kumar <prashantkumar6425@gmail.com>
2025-06-02 23:42:41 +0530

    add PostForm component

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,6 +1,6 @@
 import {RTE, Button, Input, Select} from "../index";
 import appwriteService from '../../appwrite/config';
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import React, { useCallback } from "react";
 import { useSelector } from "react-redux";
@@ -100,6 +100,49 @@ const PostForm = ({post}) => {
                 className="mb-4"
                 {...register("title", {required: true})}
             />
+            <Input 
+                label="Slug: "
+                placeholder="Slug"
+                className="mb-4"
+                {...register("slug", {required: true})}
+                onInput={(e)=>{
+                    setValue("slug",slugTransform(e.currentTarget.value), {shouldValidate: true})
+                }}
+            />
+
+                <RTE label="Content :"name="content" control={control} defaultValue={getValues("content")} />
+        </div>
+
+        <div className="w-1/3 px-2">
+                <Input 
+                    label="Featured Image: "
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg, image/gif"
+                    {...register("image", {required: !post})}
+                />
+                {post && (
+                    <div className="w-full mb-4">
+                        <img 
+                            src={appwriteService.getFilePreview(post.featuredImage)} 
+                            alt={post.title} 
+                            className="rounded-lg" 
+                        />
+
+                    </div>
+                )}
+                <Select 
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    {...register("status", {required: true})}
+                />
+                <Button 
+                    type="submit"
+                    bgColor={post ? "bg-green-500" : undefined}
+                    className="w-full"
+                {post ? "Update" : "Submit"}
+                ></Button>
         </div>
       </form>
     </div>
